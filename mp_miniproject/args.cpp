3b76f3dda843cc56ef44dc07c0c300dfee1a474d
Fangyuan Zheng <fangyuan@Fangyuans-MacBook-Pro.local>
2018-12-27 22:39:00 -0500

    remove unncessary files

--- mp_miniproject/args.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-#include <cstdlib>
-#include <iostream>
-int main(int argc, char * argv[]) {
-  for (int i = 1; i < argc; i++) {
-    std::cout << argv[i] << std::endl;
-  }
-  return EXIT_SUCCESS;
-}
